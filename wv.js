37f558c709d79aeb8df05a3a19c6ba5eb2fc2811
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-05-16 22:42:29 -0500

    New js
    finally got the js/api to work using json

--- wv.js
+++ wv.js
@@ -1,27 +1,45 @@
-$(document).ready(function(){
-  $("#searchbtn").click(function(){
-    var search = $("#search").val();
-    var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchValue;
-    $.ajax({
-      url: url,
-      async: false,
-      type: "GET",
-      dataType: "json",
-      success: function(result){
-        $("#output").empty();
-        for(var i = 0; i < result[1].length; i++){
-          $("#output").append("<li id=\"bullet\"><a target=\"_blank\" href="+result[3][i]+ ">" +result[1][i] +"</a></br>"+ result[2][i]+"</li>");
-        };
-      },
-      error: function (failed){
-        alert("API FAILED")
-      }
-    })
+let result = document.getElementById("result");
+let searchItem = document.getElementById("searchItem");
+let searchInput = document.getElementById("searchInput");
 
-  })
-  $("#search").keypress(function(event){
-    if (event.which === 13){
-      $("#searchbtn").click();
-    }
-  });
+let wikiUrl = "https://en.wikipedia.org/w/api.php?action=query&generator=search&format=json&formatversion=2&utf8=1&origin=*&gsrnamespace=0&gsrlimit=10&prop=extracts&exsentences=1&exlimit=10&explaintext=1&exintro=1&gsrsearch="
+let linkUrl = "https://en.wikipedia.org/?curid=";
+
+searchInput.addEventListener("keyup", function(event) {
+	console.log("enter listerner");
+	event.preventDefault();	
+	if (event.keyCode === 13) {
+		console.log("enter key triggered");
+		document.getElementById("btn").click();
+	}
 });
+
+function getArticle() {	
+	result.innerHTML = "";
+	let text = searchItem.elements[0].value;
+  let searchTerm = text.replace(/\s+/g,'%20');	
+	let searchUrl = wikiUrl + searchTerm;
+  	
+	let article = new XMLHttpRequest();
+	article.open('GET', searchUrl);	
+	article.onload = function() {
+	  let articleData = JSON.parse(article.responseText);
+		wikiInfo(articleData);
+    };
+	article.send();	
+}
+  
+function wikiInfo(data) {	
+	for (let i = 0; i < data.query.pages.length; i++) {
+		let url = linkUrl + data.query.pages[i].pageid;    
+		let wrapper = document.createElement("a");		
+		let div = document.createElement("div");    
+		div.setAttribute("class", "outputItem");
+		wrapper.setAttribute("href", url);
+		wrapper.setAttribute("target", "_blank");		
+		let divString = '<h3>' + data.query.pages[i].title + '</h3>' + '<p>' + data.query.pages[i].extract + '</p>';
+		div.innerHTML = divString;
+    wrapper.appendChild(div);
+		result.appendChild(wrapper);
+  }
+}
